45edab5b1431c2451239a68c9fd3c91fcdc54aad
Harasisco <k364122@gmail.com>
2023-09-29 06:26:02 -0500

    Upload the projects files

---
+++ django-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/harasisco/Desktop/DevOps_Projects/Dockerize_Python_djangoApp/django-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
